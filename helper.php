0d4401bec1cf42a160edf07acfe486b009bce262
Islam H ALAlfy <islam@alalfy.com>
2024-04-30 20:53:14 +0300

    Add dd function for dumping PHP vars to page
    Add dd function to helper.php for dumping PHP vars to the page. This function
    accepts variables as parameters and displays them in a preformatted block on the
    page.

--- helper.php
+++ helper.php
@@ -1,5 +1,9 @@
 <?php
 
+/*
+* Dump php vars to page
+* @param $vars
+*/
 function dd(...$vars) {
     echo '<pre style="background-color: #f5f5f5; padding: 10px; border: 1px solid #ccc; border-radius: 5px; margin-bottom: 20px; font-family: monospace;">';
     foreach ($vars as $varValue) {
